74f092d8dfa3333607a61bb88f210042303b3168
Mauricio Alvarez <113068974+MauricioAlvarez21@users.noreply.github.com>
2022-09-28 01:21:57 +0000

    print response

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    Scanner Response = new Scanner(System.in);
     System.out.println("Enter a word:");
-    
+
+    String word =  Response.nextLine();
+    System.out.println(word);
+      
     //Your code will return the length of the entered String
   }
 }
